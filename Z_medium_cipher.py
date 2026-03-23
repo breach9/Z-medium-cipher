8646628e2642d1e091223d0e20568dc643262edb
breach9 <g2827542@gmail.com>
2026-03-23 11:28:00 +0200

    Add medium cipher implementation in Z_medium_cipher.py
    This script implements a medium cipher that allows users to input a letter and receive the corresponding cipher text. It includes error handling for unsupported inputs.

---
+++ Z_medium_cipher.py
@@ -0,0 +1,118 @@
+from colorama import Fore
+a="@™©=¢™=©✓€={%°"
+b="#&+$:+$!$:#(=©✓¥÷€✓¥€{™€×€¥™{€✓¥="
+c="$%™%™©^€°¢=€^×`✓π€§¢¥✓"
+d="_%®✓¢°¢[×¥×{`§~{€™`{¢×"
+e="&¥¢°€®€=€®€=©[€°€✓=€™€×¥°€=]"
+f="-©™©°€✓€=€=€™¥{¥¥™=€×€™€¥=°"
+g="+°%™€=€™©×€¥™€}¥°€[©§¢√€"
+h="(€™©✓©™€=[€{€€"
+i=")✓©=€✓¥=€✓€=¥✓¥✓"
+j="/=€€™€×€✓¥°€{¥™=€€™{"
+k="*®€=%✓©÷€°¥÷¢[©™€{€=¥"
+l=":=€™©=¢™¥=€®¢=©^€{™©°€"
+m=";™€=€™€÷¥°€^¥=€™€=€✓€✓"
+n="!°%™€{€™©=%[©™%[©™€°"
+o="?°©=©™©=€✓€¥€§€✓€§€"
+p="~✓[✓[™=¢✓©÷€^¢=%^¢÷©°}"
+q="`%®=¢™€=€€®=€€™€°©™"
+r="|®%=¢™€=`®€×€÷€§×§×§§§§"
+s="•©✓©™°%°©™%{%™©°©"
+t="√√√√%°©°%=©™%=©✓√"
+u="π✓%™=€{¢€™×¢¢✓€=€="
+v="÷™©=€™€π€€✓%[€]©✓€{"
+w="®©™©®©✓©™©✓©™©=€®€÷€=×"
+x="§©™©™¥%°¢©%✓%✓%]©[©[✓©"
+y="∆%✓^¥=€✓©{©✓©=©©✓"
+z="£[©™%[^€{¢^¢×|=^€÷¢=€°"
+
+print("welcome to z medium cipher")
+print("by youssef mohammed")
+
+while True:
+    try:
+        cipher = input("Enter the alphabet you want to cipher: ")
+        
+        if cipher == "a":
+            print("cipher is:")
+            print(a)
+        elif cipher == "b":
+            print("cipher is:")
+            print(b)
+        elif cipher == "c":
+            print("cipher is:")
+            print(c)
+        elif cipher == "d":
+            print("cipher is:")
+            print(d)
+        elif cipher == "e":
+            print("cipher is:")
+            print(e)
+        elif cipher == "f":
+            print("cipher is:")
+            print(f)
+        elif cipher == "g":
+            print("cipher is:")
+            print(g)
+        elif cipher == "h":
+            print("cipher is:")
+            print(h)
+        elif cipher == "i":
+            print("cipher is:")
+            print(i)
+        elif cipher == "j":
+            print("cipher is:")
+            print(j)
+        elif cipher == "k":
+            print("cipher is:")
+            print(k)
+        elif cipher == "l":
+            print("cipher is:")
+            print(l)
+        elif cipher == "m":
+            print("cipher is:")
+            print(m)
+        elif cipher == "n":
+            print("cipher is:")
+            print(n)
+        elif cipher == "o":
+            print("cipher is:")
+            print(o)
+        elif cipher == "p":
+            print("cipher is:")
+            print(p)
+        elif cipher == "q":
+            print("cipher is:")
+            print(q)
+        elif cipher == "r":
+            print("cipher is:")
+            print(r)
+        elif cipher == "s":
+            print("cipher is:")
+            print(s)
+        elif cipher == "t":
+            print("cipher is:")
+            print(t)
+        elif cipher == "u":
+            print("cipher is:")
+            print(u)
+        elif cipher == "v":
+            print("cipher is:")
+            print(v)
+        elif cipher == "w":
+            print("cipher is:")
+            print(w)
+        elif cipher == "x":
+            print("cipher is:")
+            print(x)
+        elif cipher == "y":
+            print("cipher is:")
+            print(y)
+        elif cipher == "z":
+            print("cipher is:")
+            print(z)
+        else:
+            print("!!! the thing you typed" + Fore.GREEN + f" ({cipher})" + Fore.WHITE + " is unsupported !!!")
+            
+    except:
+        print("error in the program")
